1a1415576cf0f8d91c56e812303b8040cad20acc
GabrielHespanhol <gabriellopeshespanhol@gmail.com>
2023-10-07 14:04:26 -0300

    Enhancement get cpu metrics

--- system_monitor.py
+++ system_monitor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 """
-Python 3 - System monitor
+Python 3 - Linux prometheus exporter
 """
 
-__version__="0.1.1"
+__version__="0.1.2"
 __author__="Gabriel Hespnhol"
 
 import psutil
@@ -21,27 +21,23 @@ def memory_ram():
 def cpu_usage():
     return dict(psutil.cpu_times_percent(interval=0.5)._asdict())
 
-def update_linux_metrics():
+def update_metrics():
     try:
         """
-        Atualiza as métricas
+        Metrics update
         """
-        cpu_percent_user = Gauge('cpu_percent_user', 'CPU user percent')
-        cpu_percent_nice = Gauge('cpu_percent_nice', 'CPU nice percent')
-        cpu_percent_system = Gauge('cpu_percent_system', 'CPU system percent')
-        cpu_percent_idle = Gauge('cpu_percent_idle', 'CPU idle percent')
-        cpu_percent_iowait = Gauge('cpu_percent_iowait', 'CPU iowait percent')
-        memoria_ram_total = Gauge('ram_memory_total', 'RAM Total')
+        cpu_dict = cpu_usage()
+        for chave, valor in cpu_dict.items():
+            variable_name = "cpu_percent_" + chave
+            template = f"CPU {chave} percent"
+            vars()[variable_name] = Gauge(variable_name, template)
         while True:
             # Metricas de uso de CPU
-            cpu_percent_user.set(cpu_usage()['user'])
-            cpu_percent_nice.set(cpu_usage()['nice'])
-            cpu_percent_system.set(cpu_usage()['system'])
-            cpu_percent_idle.set(cpu_usage()['idle'])
-            cpu_percent_iowait.set(cpu_usage()['iowait'])
-            # Metricas de uso de RAM
-            memoria_ram_total.set(memory_ram()['total'])
-            time.sleep(5)
+            cpu_dict = cpu_usage()
+            for chave, valor in cpu_dict.items():
+                variable_name = "cpu_percent_" + chave
+                vars()[variable_name].set(valor)
+            time.sleep(0.1)
     except Exception as e:
         print("Problemas para atualizar as métricas! \n\n====> %s \n" % e)
         raise e
@@ -62,7 +58,7 @@ def main():
     try:
         start_exporter()
         print('Exporter Iniciado')
-        update_linux_metrics()
+        update_metrics()
     except Exception as e:
         print('\nExporter Falhou e Foi Finalizado! \n\n======> %s\n' % e)
         exit(1)
